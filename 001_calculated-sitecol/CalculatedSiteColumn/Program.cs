5ad9e88add7aef0d94a6296be0a3e4f327c61e48
Anatoly Mironov <mirontoli@gmail.com>
2015-11-17 07:37:41 +0100

    challenge 001

---
+++ 001_calculated-sitecol/CalculatedSiteColumn/Program.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.SharePoint.Client;
+using SPMeta2.CSOM.Services;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.Fields;
+using SPMeta2.Enumerations;
+using SPMeta2.Models;
+using SPMeta2.Syntax.Default;
+using System;
+using System.Collections.ObjectModel;
+
+namespace CalculatedSiteColumn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var siteUrl = "http://dev";
+            var context = new ClientContext(siteUrl);
+            var provisionService = new CSOMProvisionService();
+            var model = GetModel();
+            provisionService.DeployWebModel(context, model);
+        }
+
+        /// <summary>
+        /// 1033 and 1053 works
+        /// Space in Title works
+        /// IF statement works in 1033
+        /// </summary>
+        /// <returns></returns>
+        private static ModelNode GetModel()
+        {
+            return SPMeta2Model.NewWebModel(hostWeb =>
+            {
+                hostWeb
+                    .AddWeb(SimpleWeb, web =>
+                    {
+                        web
+                        .AddField(SimpleTextField)
+                        .AddField(SimpleCalculatedField)
+                        .AddContentType(SimpleContenType, ct =>
+                        {
+                            ct
+                                .AddContentTypeFieldLink(SimpleTextField)
+                                .AddContentTypeFieldLink(SimpleCalculatedField);
+                        })
+                        .AddList(SimpleList, list =>
+                        {
+                            list
+                                .AddContentTypeLink(SimpleContenType);
+                        });
+                    });
+            });
+        }
+        
+        private static string group = "Learn SPMeta2";
+        private static string subWebUrl = "calc09";
+        private static uint lcid = 1033;
+        private static string simpleTextFieldInternalName = "Tolle_SimpleTextField";
+        private static string simpleTextFieldTitle = "Simple Text Field";
+        private static string calculatedInternalName = "Tolle_Calculated";
+        private static string quote = "&quot;";
+
+        private static WebDefinition SimpleWeb = new WebDefinition
+        {
+            Title = subWebUrl,
+            Description = "",
+            Url = subWebUrl,
+            LCID = lcid,
+            WebTemplate = BuiltInWebTemplates.Collaboration.TeamSite
+        };
+
+        /// <summary>
+        /// A simple model
+        /// </summary>
+
+        /// <summary>
+        /// A simple text site column
+        /// </summary>
+        private static TextFieldDefinition SimpleTextField = new TextFieldDefinition
+        {
+            Id = Guid.NewGuid(),
+            Title = simpleTextFieldTitle,
+            InternalName = "Tolle_SimpleTextField",
+            Group = group,
+            Description = "",
+            AddToDefaultView = true,
+        };
+
+        /// <summary>
+        /// A simple calculated field
+        /// </summary>
+        private static CalculatedFieldDefinition SimpleCalculatedField = new CalculatedFieldDefinition
+        {
+            Id = Guid.NewGuid(),
+            Title = "Calculated Field",
+            InternalName = calculatedInternalName,
+            Group = group,
+            Required = false,
+            FieldReferences = new Collection<string> { $"{simpleTextFieldInternalName}" },
+            Formula = $"=IF([{simpleTextFieldInternalName}]={quote}hello{quote}, 1)",
+            OutputType = BuiltInFieldTypes.Number,
+            AddFieldOptions = BuiltInAddFieldOptions.AddFieldInternalNameHint,
+            AddToDefaultView = true
+        };
+
+        /// <summary>
+        /// A simple content type, that has a simple text site column and a simple calculated site column
+        /// </summary>
+        public static ContentTypeDefinition SimpleContenType = new ContentTypeDefinition
+        {
+            Name = "Simple Content Type",
+            Id = Guid.NewGuid(),
+            ParentContentTypeId = BuiltInContentTypeId.Item,
+            Group = group
+        };
+
+        /// <summary>
+        /// A simple list that should have the simple content type
+        /// </summary>
+        public static ListDefinition SimpleList = new ListDefinition
+        {
+            Title = "Simple List",
+            CustomUrl = "Lists/SimpleList",
+            ContentTypesEnabled = true,
+            TemplateType = BuiltInListTemplateTypeId.GenericList,
+            OnQuickLaunch = false
+        };
+    }
+}
---
+++ 001_calculated-sitecol/README.md
@@ -0,0 +1,20 @@
+# Challenge 001 - Calculated Site Column
+
+## goal and scenario
+Provision a calculated field in a content type that is added to a list.
+
+##how far we've come
+Calculated columns are lost on the list when the formula contains parts that change in localized versions. In plain text, it is not present in the list (though it is there in the content type) if you try to use formula =IF([SomeField]="hello",1) on a Swedish site. It works fine in an English site. The formula gets converted to =OM([SomeField]="hello";1) on a Swedish site (in other lcid it is different). Don't mind the formula, it is just the simplest test. 
+
+What works is:
+- All formulas that don't change on localization
+- Any formula (I suppose) on English sites (LCID=1033)
+
+
+## implementation
+1. Add a text column
+2. Add a calculated column
+3. Add a new content type
+4. Add our two columns to that content type
+5. Add a generic list
+6. Add our content type to our list
